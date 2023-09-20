ff910f60ed591ebc5150c49cbd66a16328f01e21
Freddy Esteban Perez <fredstban@icloud.com>
2023-09-20 08:59:04 -0500

    Set mapping for toggle term

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -2,5 +2,9 @@
 return {
   "akinsho/toggleterm.nvim",
   version = "*",
-  config = true
+  config = function ()
+    require("toggleterm").setup({
+      open_mapping = [[<c-\>]],
+    })
+  end
 }
