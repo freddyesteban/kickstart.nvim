b559e3bc3233724864c0d13a350cafeec55b17ed
Freddy Esteban Perez <fredstban@icloud.com>
2023-09-21 05:34:57 -0500

    Fix bufferline tabs

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -5,5 +5,41 @@ return {
   version = "main",
   event = { "BufRead", "BufWinEnter", "BufNewFile" },
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = true
+  config = function()
+    require("bufferline").setup({
+      options = {
+        offsets = {
+          {
+            filetype = "undotree",
+            text = "Undotree",
+            highlight = "PanelHeading",
+            padding = 1,
+          },
+          {
+            filetype = "NvimTree",
+            text = "Explorer",
+            highlight = "PanelHeading",
+            padding = 1,
+          },
+          {
+            filetype = "DiffviewFiles",
+            text = "Diff View",
+            highlight = "PanelHeading",
+            padding = 1,
+          },
+          {
+            filetype = "flutterToolsOutline",
+            text = "Flutter Outline",
+            highlight = "PanelHeading",
+          },
+          {
+            filetype = "lazy",
+            text = "Lazy",
+            highlight = "PanelHeading",
+            padding = 1,
+          },
+        }
+      }
+    })
+  end
 }
