e3aa57af77f79ea29d8541f857a783b7e6c29e3b
Freddy Esteban Perez <fredstban@icloud.com>
2023-09-22 05:55:09 -0500

    Add project and configure with nvim-tree

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -4,6 +4,13 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   event = { "BufRead", "BufWinEnter", "BufNewFile" },
   config = function()
-    require("nvim-tree").setup()
+    require("nvim-tree").setup({
+      sync_root_with_cwd = true,
+      respect_buf_cwd = true,
+      update_focused_file = {
+        enable = true,
+        update_root = true
+      },
+    })
   end
 }
---
+++ lua/custom/plugins/project.lua
@@ -0,0 +1,10 @@
+-- Provides project management
+-- https://github.com/ahmedkhalf/project.nvim
+return {
+  "ahmedkhalf/project.nvim",
+  config = function()
+    require("project_nvim").setup()
+  end,
+  event = "VimEnter",
+  cmd = "Telescope projects",
+}
